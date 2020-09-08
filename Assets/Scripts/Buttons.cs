7bcfbb8585f63abee16b9d442dc7ffb73046175f
Дмитрий Шаров <overlord.shor@gmail.com>
2020-09-08 23:44:02 +0300

    Добавлены методы реагирования на нажатие кнопок

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,7 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Buttons : MonoBehaviour
 {
+    private Vector3 originalScale;
+    private Vector3 pressedScale;
+
+    private void Start()
+    {
+        originalScale = new Vector3(0.01915709f, 0.01915709f);
+        pressedScale = new Vector3(0.027f, 0.027f);
+    }
+
+    private void OnMouseDown()
+    {
+        transform.localScale = pressedScale;
+    }
+
+    private void OnMouseUp()
+    {
+        transform.localScale = originalScale;
+    }
 }
