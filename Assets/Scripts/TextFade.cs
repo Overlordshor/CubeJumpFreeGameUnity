1f2d8cbf854fe02764c987919f939db78e507bd2
Дмитрий Шаров <overlord.shor@gmail.com>
2020-09-09 18:43:29 +0300

    Добавил мигание текста TAP TO PLAY

---
+++ Assets/Scripts/TextFade.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class TextFade : MonoBehaviour
+{
+    private Text text;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.PingPong(Time.time, 1f));
+    }
+}
