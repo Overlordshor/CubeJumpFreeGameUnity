e4c9adf0d2008de7f5c908c14d51be6878faf08c
Дмитрий Шаров <overlord.shor@gmail.com>
2020-09-09 21:51:49 +0300

    Добавлено очищение экрана, анимация куба в стартовую позицию

--- Assets/Scripts/MainScene/Buttons.cs
+++ Assets/Scripts/MainScene/Buttons.cs
@@ -1,14 +1,35 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Buttons : MonoBehaviour
 {
     private Vector3 originalScale;
     private Vector3 pressedScale;
+    private float speed;
+    private bool move;
+    private RectTransform button;
+
+    public void GoAway()
+    {
+        speed = 5f;
+        move = true;
+        Destroy(gameObject, 1f);
+    }
 
     private void Start()
     {
         originalScale = new Vector3(0.01915709f, 0.01915709f);
         pressedScale = new Vector3(0.027f, 0.027f);
+        button = GetComponent<RectTransform>();
+        move = false;
+    }
+
+    private void Update()
+    {
+        if (move)
+        {
+            button.offsetMin -= new Vector2(0, speed);
+        }
     }
 
     private void OnMouseDown()
---
+++ Assets/Scripts/MainScene/ClickDetector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickDetector : MonoBehaviour
+{
+    public Text GameNameText, PlayGameText;
+    public Buttons buttons;
+    public GameObject MainCube;
+
+    private void OnMouseDown()
+    {
+        GameNameText.text = "0";
+        PlayGameText.gameObject.SetActive(false);
+        buttons.GoAway();
+        MainCube.GetComponent<Animation>().Play("StartGameCube");
+    }
+}
