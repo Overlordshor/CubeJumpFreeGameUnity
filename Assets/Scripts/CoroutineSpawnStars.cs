4edfe42dbd33ab4c14f2f036fdc0920b187a7cd9
Дмитрий Шаров <overlord.shor@gmail.com>
2020-09-09 19:22:08 +0300

    Добавил появление звездочек на главном экране

---
+++ Assets/Scripts/CoroutineSpawnStars.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoroutineSpawnStars : MonoBehaviour
+{
+    public GameObject Star;
+
+    private void Start()
+    {
+        StartCoroutine("spawn");
+    }
+
+    private IEnumerator spawn()
+    {
+        while (true)
+        {
+            Vector3 position = Camera.main.ScreenToWorldPoint(
+                new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height),
+                Camera.main.farClipPlane / 2));
+
+            Instantiate(Star, position, Quaternion.identity);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
