614fd18f1afbc92eb0cb1b28c259fd0beaa788ae
Дмитрий Шаров <overlord.shor@gmail.com>
2020-09-28 21:07:35 +0300

    Добавил сохранение лучшего результата

--- Assets/Scripts/Game/Score.cs
+++ Assets/Scripts/Game/Score.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     public Text GameNameText;
+    public Text Record;
 
     private int totalScore = 0;
     private int buffPower;
@@ -13,10 +14,25 @@ public class Score : MonoBehaviour
     {
         totalScore += 1 * buffPower;
         GameNameText.text = Convert.ToString(totalScore);
+        SetRecord(totalScore);
     }
 
     public void RefreshBuff(int heightTower)
     {
         buffPower = heightTower - 2;
+    }
+
+    private void Start()
+    {
+        SetRecord(totalScore);
+    }
+
+    private void SetRecord(int totalScore)
+    {
+        if (PlayerPrefs.GetInt("Record") < totalScore)
+        {
+            PlayerPrefs.SetInt("Record", totalScore);
+        }
+        Record.text = "TOP: " + PlayerPrefs.GetInt("Record").ToString();
     }
 }
