e9fe18600109ec91aa5a22fa3023b39a7c947faf
Дмитрий Шаров <overlord.shor@gmail.com>
2020-10-03 22:24:39 +0300

    Добавил описание новой функции

--- Assets/Scripts/MainScene/Language.cs
+++ Assets/Scripts/MainScene/Language.cs
@@ -3,6 +3,12 @@ using UnityEngine.UI;
 
 public class Language
 {
+    /// <summary>
+    /// In the presented text on the stage, chooses which text to submit depending on the player's settings.
+    /// </summary>
+    /// <param name="text"></param>
+    /// <param name="english"></param>
+    /// <param name="russian"></param>
     public static void PrintAnyLanguage(Text text, string english, string russian)
     {
         switch (PlayerPrefs.GetString("Language"))
