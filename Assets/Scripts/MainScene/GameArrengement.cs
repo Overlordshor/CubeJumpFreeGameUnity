f40c0ac62dc4c98a1bce4e5d27bea8d025d78717
Дмитрий Шаров <overlord.shor@gmail.com>
2020-11-04 21:23:37 +0300

    Правка ложного нажатия начала игры

--- Assets/Scripts/MainScene/GameArrengement.cs
+++ Assets/Scripts/MainScene/GameArrengement.cs
@@ -39,7 +39,7 @@ public class GameArrengement : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
+            if (touch.phase == TouchPhase.Ended)
             {
                 SwtichTextsScene();
 
