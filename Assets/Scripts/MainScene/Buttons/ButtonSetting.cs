dcbb0140db48603741e71c37e6682f96c6708166
Дмитрий Шаров <overlord.shor@gmail.com>
2020-11-18 20:16:10 +0300

    Добавил скрипт на кнопки настройки

---
+++ Assets/Scripts/MainScene/Buttons/ButtonSetting.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ButtonSetting : MonoBehaviour
+{
+    public GameObject SocialGroupBar;
+
+    private void OnEnable()
+    {
+        ToggleUI(SocialGroupBar);
+    }
+
+    private void OnDisable()
+    {
+        ToggleUI(SocialGroupBar);
+    }
+
+    private void ToggleUI(GameObject gameObject)
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
