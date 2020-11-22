fd8434bae2a433f314f676f5dc29b5b9291ad326
Дмитрий Шаров <overlord.shor@gmail.com>
2020-11-22 21:58:09 +0300

    Переработка магазина, начало работы

---
+++ Assets/Scripts/MainScene/Buttons/Shop/CubeSelection.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CubeSelection : MonoBehaviour
+{
+    public GameObject Cubes, MainCube;
+    private new AudioSource audio;
+    private Material selectMaterial;
+
+    private void Start()
+    {
+        audio = GetComponentInParent<AudioSource>();
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        audio.Play();
+
+        selectMaterial = gameObject.GetComponent<MeshRenderer>().material;
+        PlayerPrefs.SetString("Skin", selectMaterial.name);
+        var originalScale = transform.localScale;
+        transform.localScale += new Vector3(50f, 50f, 50f);
+    }
+}
